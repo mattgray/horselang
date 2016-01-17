f708fb935100413863d0e43d503325789e2b0a8f
Matt Gray <matthew.thomas.gray@gmail.com>
2016-01-17 13:34:33 +0000

    Add more horselang

--- horselang.ml
+++ horselang.ml
@@ -9,7 +9,15 @@ let peek (source, pos) = String.get source pos
 
 let string_of_char = Printf.sprintf "%c"
 
-type token = Identifier of string | Token of string | EOF
+type token =
+  Identifier of string
+  | Assignment
+  | EndStatement
+  | Operator of char
+  | String of string
+  | Number of float
+  | EOF
+
 exception Horselang_parse_error of string
 
 let whitespace = [ '\n'; '\t'; ' ' ]
@@ -18,17 +26,37 @@ let gen_alpha () =
   for x = 97 to 122 do
     alpha := List.append !alpha [Char.chr x]
   done;
+  for x = 65 to 90 do
+    alpha := List.append !alpha [Char.chr x]
+  done;
   !alpha
 
 let alpha = gen_alpha ()
 
+let gen_num () =
+  let num = ref [] in
+  for x = 48 to 57 do
+    num := List.append !num [Char.chr x]
+  done;
+  !num
+
+let numeric = gen_num ()
+
+let operators = ['+'; '-'; '/'; '*'; '|']
+
 let rec read_token s =
   try
     match read s with
-      | (c, s) when List.mem c whitespace -> read_token s
-      | (c, s) when List.mem c alpha -> read_identifier [string_of_char c] s
-      | ('/', s) when (peek s = '*') -> read_comment s
-      | (c, s) -> (Token (string_of_char c), s)
+      | '/', s when (peek s = '*') -> read_comment s
+      | c, s when List.mem c whitespace -> read_token s
+      | c, s when List.mem c alpha -> read_identifier [string_of_char c] s
+      | c, s when List.mem c numeric -> read_numeric [string_of_char c] s
+      | c, s when List.mem c operators -> Operator c, s
+      | '"', s -> read_string [] s
+      | '=', s -> Assignment, s
+      | ';', s -> EndStatement, s
+      | c, s -> raise @@
+        Horselang_parse_error (Printf.sprintf "Unexpected %c" c)
   with Invalid_argument _ -> (EOF, s)
 and read_comment s =
   try
@@ -39,14 +67,29 @@ and read_comment s =
 and read_identifier ident s =
   try
     match read s with
-      | (c,s) when List.mem c whitespace -> (Identifier (String.concat "" ident), s)
-      | (c,s) when List.mem c alpha -> read_identifier (List.append ident [string_of_char c]) s
-      | (c, (_, pos)) -> raise (Horselang_parse_error (Printf.sprintf "unexpected %c at position %d" c pos))
+      | (c, s) when List.mem c whitespace -> (Identifier (String.concat "" ident), s)
+      | (c, s) when List.mem c alpha -> read_identifier (List.append ident [string_of_char c]) s
+      | (c, (s, pos)) ->
+        raise (Horselang_parse_error (Printf.sprintf
+          "unexpected %c at position %d (%s)" c pos s))
   with Invalid_argument _ -> (Identifier (String.concat "" ident), s)
-
-let keywords = [ "if"; "then"; "else"; "elif"; "endif"; "while"; "loop"; "endloop"; "print"; "return"; "exit"; ]
-let symbols = [ "="; "("; ")"; "<"; ">"; "/"; "*"; "+"; "-"; "!"; "&"; "."; ";"; ]
-let two_char_symbols = [ "=="; "<="; ">="; "<>"; "!="; "++"; "**"; "--"; "+="; "-="; "||"; ]
+and read_string thestring s =
+  try
+    match read s with
+      | ('"', s) -> (String (String.concat "" thestring), s)
+      | (c, s) -> read_string (List.append thestring [string_of_char c]) s
+  with Invalid_argument _ -> raise @@ Horselang_parse_error "Unterminated string literal"
+and read_numeric thenumberstring s =
+  try
+    match read s with
+      | c, s when List.mem c numeric ->
+          read_numeric (List.append thenumberstring [string_of_char c]) s
+      | c, s when List.mem c whitespace ->
+          ((Number (float_of_string (String.concat "" thenumberstring))), s)
+      | (c, (s, pos)) ->
+        raise (Horselang_parse_error (Printf.sprintf
+          "unexpected %c in number literal at  %d (%s)" c pos s))
+  with Invalid_argument _ -> raise @@ Horselang_parse_error "Unterminated number literal"
 
 let file_read_all : in_channel -> string = fun chan ->
   let lines = ref [] in
@@ -68,8 +111,12 @@ let get_tokens src =
   get_tokens src []
 
 let debug_token = function
-  | Token c -> Printf.sprintf "Token %s" c
+  | String s -> Printf.sprintf "String \"%s\"" s
+  | Number f -> Printf.sprintf "Number %f" f
+  | Assignment -> Printf.sprintf "Assignment ="
+  | EndStatement -> Printf.sprintf "EndStatement ;"
   | Identifier name -> Printf.sprintf "Identifier %s" name
+  | Operator c -> Printf.sprintf "Operator %s" (string_of_char c)
   | EOF -> "EOF"
 
 let () =
