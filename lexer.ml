b3db003ab87926f6dc2535ea32e2fb70410b9263
Matt Gray <matthew.thomas.gray@gmail.com>
2016-02-08 22:14:07 +0000

    Split Lexer into seperate module

---
+++ lexer.ml
@@ -0,0 +1,71 @@
+let range i j =
+  let rec aux n acc =
+    if n < i then acc else aux (n-1) (n :: acc)
+  in aux j []
+
+let alpha = List.map Char.chr (range 65 90) @ List.map Char.chr (range 97 122)
+let numeric = List.map Char.chr (range 48 57)
+let operators = ['+'; '-'; '/'; '*'; '|']
+let whitespace = [ '\n'; '\t'; ' ' ]
+
+let string_of_char = Printf.sprintf "%c"
+
+type token =
+  Identifier of string
+  | Assignment
+  | EndStatement
+  | Operator of char
+  | String of string
+  | Number of float
+  | EOF
+
+exception Horselang_parse_error of string
+
+let rec read_token s =
+  match Scanner.next s with
+    | '/' when (Scanner.peek s = (Some '*')) -> read_comment s
+    | c when List.mem c whitespace -> read_token s
+    | c when List.mem c alpha -> read_identifier [string_of_char c] s
+    | c when List.mem c numeric -> read_numeric [string_of_char c] s
+    | c when List.mem c operators -> Operator c
+    | '"' -> read_string [] s
+    | '=' -> Assignment
+    | ';' -> EndStatement
+    | c -> raise @@ Horselang_parse_error (Printf.sprintf "Unexpected %c" c)
+and read_comment s =
+  match Scanner.next s with
+    | '*' when Scanner.peek s = (Some '/') -> let _ = Scanner.next s in read_token s
+    | _ -> read_comment s
+and read_identifier ident s =
+  try
+    match Scanner.next s with
+      | c when List.mem c whitespace -> Identifier (String.concat "" ident)
+      | c when List.mem c alpha -> read_identifier (List.append ident [string_of_char c]) s
+      | c -> raise @@ Horselang_parse_error "Unexpected identifier blah blah"
+  with Stream.Failure -> Identifier (String.concat "" ident)
+and read_string thestring s =
+  try
+    match Scanner.next s with
+      | '"' -> String (String.concat "" thestring)
+      | c -> read_string (List.append thestring [string_of_char c]) s
+  with Stream.Failure -> raise @@ Horselang_parse_error "Unterminated string literal"
+and read_numeric thenumberstring s =
+  try
+    match Scanner.next s with
+      | c when List.mem c numeric ->
+          read_numeric (List.append thenumberstring [string_of_char c]) s
+      | c when List.mem c whitespace ->
+          Number (float_of_string (String.concat "" thenumberstring))
+      | c  -> raise @@
+        Horselang_parse_error (Printf.sprintf "invalid number at line %d"
+          (Scanner.get_line s))
+  with Stream.Failure -> raise @@ Horselang_parse_error "Unterminated number literal"
+
+let debug_token = function
+  | String s -> Printf.sprintf "String \"%s\"" s
+  | Number f -> Printf.sprintf "Number %f" f
+  | Assignment -> Printf.sprintf "Assignment ="
+  | EndStatement -> Printf.sprintf "EndStatement ;"
+  | Identifier name -> Printf.sprintf "Identifier %s" name
+  | Operator c -> Printf.sprintf "Operator %s" (string_of_char c)
+  | EOF -> "EOF"
