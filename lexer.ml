2c9717c9e7514d2ded3556e65b26c7900a3c6ce2
Matt Gray <matthew.thomas.gray@gmail.com>
2016-02-14 19:28:54 +0000

    Start following the kaleidoscope LLVM tutorial

--- lexer.ml
+++ lexer.ml
@@ -1,3 +1,9 @@
+type token =
+  Def | Extern
+  | Ident of string
+  | Number of float
+  | Kwd of char
+
 let range i j =
   let rec aux n acc =
     if n < i then acc else aux (n-1) (n :: acc)
@@ -5,17 +11,7 @@ let range i j =
 
 let alpha = List.map Char.chr (range 65 90) @ List.map Char.chr (range 97 122)
 let numeric = List.map Char.chr (range 48 57)
-let operators = ['+'; '-'; '/'; '*'; '|']
 let whitespace = [ '\n'; '\t'; ' ' ]
-
-type token =
-  Identifier of string
-  | Assignment
-  | EndStatement
-  | Operator of char
-  | String of string
-  | Number of float
-  | EOF
 
 exception Horselang_parse_error of string
 
@@ -23,50 +19,35 @@ let buffer_from c = let buffer = Buffer.create 16 in Buffer.add_char buffer c; b
 
 let rec read_token s =
   match Scanner.next s with
-    | '/' when (Scanner.peek s = (Some '*')) -> read_comment s
-    | c when List.mem c whitespace -> read_token s
-    | c when List.mem c alpha -> read_identifier (buffer_from c) s
-    | c when List.mem c numeric -> read_numeric (buffer_from c) s
-    | c when List.mem c operators -> Operator c
-    | '"' -> read_string (Buffer.create 16) s
-    | '=' -> Assignment
-    | ';' -> EndStatement
-    | c -> raise @@ Horselang_parse_error (Printf.sprintf "Unexpected %c" c)
+  | '#' -> read_comment s
+  | c when List.mem c whitespace -> read_token s (* skip over whitespace *)
+  | c when List.mem c alpha -> read_identifier (buffer_from c) s
+  | c when List.mem c numeric -> read_numeric (buffer_from c) s
+  | c -> Kwd c
 and read_comment s =
   match Scanner.next s with
-    | '*' when Scanner.peek s = (Some '/') -> let _ = Scanner.next s in read_token s
-    | _ -> read_comment s
+  | '\n' -> read_token s
+  | _ -> read_comment s
 and read_identifier buf s =
-  try
-    match Scanner.next s with
-      | c when List.mem c whitespace -> Identifier (Buffer.contents buf)
-      | c when List.mem c alpha -> Buffer.add_char buf c; read_identifier buf s
-      | c -> raise @@ Horselang_parse_error "Unexpected identifier blah blah"
-  with Stream.Failure -> Identifier (Buffer.contents buf)
-and read_string buf s =
-  try
-    match Scanner.next s with
-      | '"' -> String (Buffer.contents buf)
-      | c -> Buffer.add_char buf c; read_string buf s
-  with Stream.Failure -> raise @@ Horselang_parse_error "Unterminated string literal"
+  match Scanner.peek s with
+  | c when List.mem c alpha -> Buffer.add_char buf (Scanner.next s); read_identifier buf s
+  | c when List.mem c whitespace ->
+    (let _ = Scanner.next s in match (Buffer.contents buf) with
+    | "def" -> Def
+    | "extern" -> Extern
+    | ident -> Ident ident)
+  (* something else, just carry on, don't advance *)
+  | c -> Ident (Buffer.contents buf)
 and read_numeric buf s =
-  try
-    match Scanner.next s with
-      | c when List.mem c numeric ->
-          Buffer.add_char buf c;
-          read_numeric buf s
-      | c when List.mem c whitespace ->
-          Number (float_of_string (Buffer.contents buf))
-      | c  -> raise @@
-        Horselang_parse_error (Printf.sprintf "invalid number at line %d"
-          (Scanner.get_line s))
-  with Stream.Failure -> raise @@ Horselang_parse_error "Unterminated number literal"
+  match Scanner.peek s with
+  | c when List.mem c numeric ->
+    Buffer.add_char buf (Scanner.next s);
+    read_numeric buf s
+  | _  -> Number (float_of_string (Buffer.contents buf))
 
 let debug_token = function
-  | String s -> Printf.sprintf "String \"%s\"" s
   | Number f -> Printf.sprintf "Number %f" f
-  | Assignment -> Printf.sprintf "Assignment ="
-  | EndStatement -> Printf.sprintf "EndStatement ;"
-  | Identifier name -> Printf.sprintf "Identifier %s" name
-  | Operator c -> Printf.sprintf "Operator %c" c
-  | EOF -> "EOF"
+  | Def -> "Def"
+  | Extern -> "Extern"
+  | Ident name -> Printf.sprintf "Identifier %s" name
+  | Kwd c -> Printf.sprintf "Kwd %c" c
--- scanner.ml
+++ scanner.ml
@@ -1,5 +1,7 @@
 type t = char Stream.t * int ref
 
+exception Peeked_past_end_of_stream
+
 let of_char_stream stream = (stream, ref 1)
 
 let next (stream, line) =
@@ -7,6 +9,9 @@ let next (stream, line) =
     | '\n' -> line := !line + 1; '\n'
     | c -> c
 
-let peek (stream, _) = Stream.peek stream
+let peek (stream, _) =
+  match Stream.peek stream with
+  | Some c -> c
+  | _ -> raise Peeked_past_end_of_stream
 
 let get_line (_, line) = !line
