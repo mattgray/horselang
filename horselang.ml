bf81fcec19135566e0d46e52325f8299e75b93ef
Matt Gray <matthew.thomas.gray@gmail.com>
2016-02-14 21:33:51 +0000

    Better top level

--- horselang.ml
+++ horselang.ml
@@ -5,11 +5,18 @@ let get_tokens source =
   Stream.from
    (fun _ -> try Some (Lexer.read_token source) with Stream.Failure -> None)
 
+let rec main_loop tokens =
+  match Stream.peek tokens with
+  | None -> ()
+  | Some _ ->
+    print_endline (Lexer.debug_token (Stream.next tokens));
+    print_string "horselang> ";
+    flush stdout;
+    main_loop tokens
+
 let () =
+  print_endline "Welcome to horselang v1";
+  print_string "horselang> "; flush stdout;
   let source = Scanner.of_char_stream (Stream.of_channel stdin) in
   let tokens = get_tokens source in
-  Stream.iter (
-    fun t ->
-      print_string (Lexer.debug_token t);
-      print_endline (Printf.sprintf " on line %d" (Scanner.get_line source))
-    ) tokens
+  main_loop tokens
